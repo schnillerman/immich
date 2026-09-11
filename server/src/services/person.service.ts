70d443dbabed3f17afd7bebc7f10137586f9da2f
Daniel Dietzler <mail@ddietzler.dev>
2026-09-11 19:29:45 +0200

    feat: people merge improvements

--- server/src/services/person.service.ts
+++ server/src/services/person.service.ts
@@ -578,82 +578,89 @@ export class PersonService extends BaseService {
     return JobStatus.Success;
   }
 
-  async mergePerson(auth: AuthDto, personGroupId: string, dto: MergePersonDto): Promise<BulkIdResponseDto[]> {
-    const mergeIds = dto.ids;
-    if (mergeIds.includes(personGroupId)) {
+  async mergePeople(auth: AuthDto, { ids }: MergePersonDto): Promise<BulkIdResponseDto[]> {
+    if (ids.length < 2) {
+      throw new BadRequestException('At least two people are required for merging');
+    }
+
+    if (new Set(ids).size !== ids.length) {
       throw new BadRequestException('Cannot merge a person into themselves');
     }
 
-    await this.requireAccess({ auth, permission: Permission.PersonUpdate, ids: [personGroupId] });
-
     const results: BulkIdResponseDto[] = [];
 
-    const allowedIds = await this.checkAccess({
-      auth,
-      permission: Permission.PersonMerge,
-      ids: mergeIds,
-    });
+    const allowedIds = await this.checkAccess({ auth, permission: Permission.PersonMerge, ids });
 
-    let primaryPerson: Selectable<PersonTable> | undefined;
+    const peopleMap: Record<string, Selectable<PersonTable>[]> = {};
 
-    for (const mergePerson of await this.personRepository.getForMergePerson(mergeIds)) {
-      const mergeId = mergePerson.personGroupId;
+    for (const mergePerson of await this.personRepository.getForMergePerson(ids)) {
+      if (!peopleMap[mergePerson.personGroupId]) {
+        peopleMap[mergePerson.personGroupId] = [];
+      }
+      peopleMap[mergePerson.personGroupId].push(mergePerson);
+    }
+
+    const targetPeople: Record<string, Selectable<PersonTable>> = {};
+    for (const mergeId of ids) {
       const hasAccess = allowedIds.has(mergeId);
       if (!hasAccess) {
         results.push({ id: mergeId, success: false, error: BulkIdErrorReason.NO_PERMISSION });
         continue;
       }
 
-      if (!primaryPerson || primaryPerson.ownerId !== mergePerson.ownerId) {
-        primaryPerson = await this.personRepository.getByGroupId({ ownerId: mergePerson.ownerId, personGroupId });
-        if (!primaryPerson) {
+      for (const mergePerson of peopleMap[mergeId]) {
+        if (!targetPeople[mergePerson.ownerId]) {
+          targetPeople[mergePerson.ownerId] = mergePerson;
           continue;
         }
-      }
 
-      const changes: Updateable<Person> = {};
-      if (!primaryPerson.name && mergePerson.name) {
-        changes.name = mergePerson.name;
-      }
+        const targetPerson = targetPeople[mergePerson.ownerId];
 
-      if (!primaryPerson.birthDate && mergePerson.birthDate) {
-        changes.birthDate = mergePerson.birthDate;
-      }
+        if (
+          (targetPerson.name !== mergePerson.name || targetPerson.birthDate !== mergePerson.birthDate) &&
+          mergePerson.ownerId !== auth.user.id
+        ) {
+          continue;
+        }
 
-      if (
-        (mergePerson.name && mergePerson.name !== primaryPerson.name) ||
-        (mergePerson.birthDate && mergePerson.birthDate !== primaryPerson.birthDate)
-      ) {
-        continue;
-      }
+        const changes: Updateable<Person> = {};
+        if (mergePerson.name && targetPerson.name !== mergePerson.name) {
+          changes.name = mergePerson.name;
+        }
 
-      if (Object.keys(changes).length > 0) {
-        primaryPerson = await this.personRepository.update({
-          ownerId: primaryPerson.ownerId,
-          personGroupId: primaryPerson.personGroupId,
-          ...changes,
-        });
-      }
+        if (mergePerson.birthDate && targetPerson.birthDate !== mergePerson.birthDate) {
+          changes.birthDate = mergePerson.birthDate;
+        }
 
-      const mergeName = mergePerson.name || mergePerson.personGroupId;
-      const mergeData: UpdateFacesData = {
-        oldPersonGroupId: mergeId,
-        newPersonGroupId: primaryPerson.personGroupId,
-        ownerId: primaryPerson.ownerId,
-      };
-      this.logger.log(`Merging ${mergeName} into ${primaryPerson.name || primaryPerson.personGroupId}`);
+        if (Object.keys(changes).length > 0) {
+          targetPeople[mergePerson.ownerId] = await this.personRepository.update({
+            ownerId: targetPerson.ownerId,
+            personGroupId: targetPerson.personGroupId,
+            ...changes,
+          });
+        }
 
-      try {
-        await this.personRepository.reassignFaces(mergeData);
-        await this.removeAllPersonGroups([mergeId], primaryPerson.ownerId);
+        const mergeName = mergePerson.name || mergePerson.personGroupId;
+        const mergeData: UpdateFacesData = {
+          oldPersonGroupId: mergeId,
+          newPersonGroupId: targetPerson.personGroupId,
+          ownerId: targetPerson.ownerId,
+        };
+        this.logger.log(`Merging ${mergeName} into ${targetPerson.name || targetPerson.personGroupId}`);
 
-        this.logger.log(`Merged ${mergeName} into ${primaryPerson.name || primaryPerson.personGroupId}`);
-        results.push({ id: mergeId, success: true });
-      } catch (error: any) {
-        this.logger.error(`Unable to merge ${mergeId} into ${personGroupId}: ${error}`, error?.stack);
-        results.push({ id: mergeId, success: false, error: BulkIdErrorReason.UNKNOWN });
+        try {
+          await this.personRepository.reassignFaces(mergeData);
+          await this.removeAllPersonGroups([mergeId], targetPerson.ownerId);
+
+          this.logger.log(`Merged ${mergeName} into ${targetPerson.name || targetPerson.personGroupId}`);
+          results.push({ id: mergeId, success: true });
+        } catch (error: any) {
+          this.logger.error(`Unable to merge ${mergeId} into ${targetPerson.personGroupId}: ${error}`, error?.stack);
+          results.push({ id: mergeId, success: false, error: BulkIdErrorReason.UNKNOWN });
+        }
       }
     }
+
     return results;
   }
 
--- server/test/medium/specs/services/person.service.spec.ts
+++ server/test/medium/specs/services/person.service.spec.ts
@@ -223,7 +223,7 @@ describe(PersonService.name, () => {
 
       const auth = factory.auth({ user: user1 });
 
-      await sut.mergePerson(auth, person1.personGroupId, { ids: [person2.personGroupId] });
+      await sut.mergePeople(auth, { ids: [person1.personGroupId, person2.personGroupId] });
       const user1People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user1.id }));
       const user2People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user2.id }));
       expect(user1People).toEqual([expect.objectContaining({ personGroupId: person1.personGroupId })]);
@@ -254,7 +254,7 @@ describe(PersonService.name, () => {
 
       const auth = factory.auth({ user: user1 });
 
-      await sut.mergePerson(auth, person1.personGroupId, { ids: [person2.personGroupId] });
+      await sut.mergePeople(auth, { ids: [person1.personGroupId, person2.personGroupId] });
       const user1People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user1.id }));
       const user2People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user2.id }));
       expect(user1People).toEqual([expect.objectContaining({ personGroupId: person1.personGroupId })]);
@@ -266,7 +266,7 @@ describe(PersonService.name, () => {
       );
     });
 
-    it('should skip people with a different birthdate', async () => {
+    it('should skip people with a different birth date', async () => {
       const { sut, ctx } = setup();
       const storageMock = ctx.getMock(StorageRepository);
       const { user: user1 } = await ctx.newUser();
@@ -287,7 +287,7 @@ describe(PersonService.name, () => {
 
       const auth = factory.auth({ user: user1 });
 
-      await sut.mergePerson(auth, person1.personGroupId, { ids: [person2.personGroupId] });
+      await sut.mergePeople(auth, { ids: [person1.personGroupId, person2.personGroupId] });
       const user1People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user1.id }));
       const user2People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user2.id }));
       expect(user1People).toEqual([expect.objectContaining({ personGroupId: person1.personGroupId })]);
@@ -316,13 +316,28 @@ describe(PersonService.name, () => {
 
       const auth = factory.auth({ user: user1 });
 
-      await sut.mergePerson(auth, person1.personGroupId, { ids: [person2.personGroupId] });
+      await sut.mergePeople(auth, { ids: [person1.personGroupId, person2.personGroupId] });
       const user1People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user1.id }));
       const user2People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user2.id }));
       expect(user1People).toEqual([expect.objectContaining({ personGroupId: person1.personGroupId })]);
       expect(user2People).toEqual([expect.objectContaining({ personGroupId: person2.personGroupId })]);
       await expect(ctx.get(PersonRepository).getFaces(asset.id, { viewingUserId: asset.ownerId })).resolves.toEqual([
         expect.objectContaining({ personGroupId: person2.personGroupId }),
+      ]);
+    });
+
+    it('should collapse names in order for auth user', async () => {
+      const { sut, ctx } = setup();
+      const { user } = await ctx.newUser();
+      const { person: person1 } = await ctx.newPerson({ ownerId: user.id, name: 'Person 1' });
+      const { person: person2 } = await ctx.newPerson({ ownerId: user.id, name: 'Person 2' });
+
+      const auth = factory.auth({ user });
+
+      await sut.mergePeople(auth, { ids: [person1.personGroupId, person2.personGroupId] });
+      const user1People = await Array.fromAsync(ctx.get(PersonRepository).getAll({ ownerId: user.id }));
+      expect(user1People).toEqual([
+        expect.objectContaining({ personGroupId: person1.personGroupId, name: 'Person 2' }),
       ]);
     });
   });
